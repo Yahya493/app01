9c6eaff0da33e8ec983acc1c98450c2023cd30ad
Yahya493 <78732281+Yahya493@users.noreply.github.com>
2023-04-26 14:45:43 +0300

    Optimize rating

--- src/components/ProductPage.js
+++ src/components/ProductPage.js
@@ -7,7 +7,7 @@ export default function ProductPage(props) {
   const [i, setI] = useState(0)
   let { id } = useParams()
   const api = new ApiHelper()
-  const [product, setProduct] = useState({rating:{rate:'', count: ''}})
+  const [product, setProduct] = useState({ rating: { rate: '', count: '' } })
   if (i == 0) {
     api.getProductById(id).then(data => {
       setI(i + 1)
@@ -19,7 +19,7 @@ export default function ProductPage(props) {
     <div className='container row'>
       <h3>ProductPage</h3>
       <div className='col s12 m4'>
-        <img src={product.image} width={'100%'}/>
+        <img src={product.image} width={'100%'} />
       </div>
       <div className='col s12 m8'>
         <h4>{product.title}</h4>
@@ -27,19 +27,18 @@ export default function ProductPage(props) {
         <h5>${product.price}</h5>
         {/* <h5>Rate: {product.rating.rate} {product.rating.count}</h5> */}
         <div>
-      {[...Array(5)].map((_, i) => {
-        const ratingValue = i + 1;
-        return (
-          <span key={i} >
-            <input type="radio" name="rating" value={ratingValue} />
-            <label className={ratingValue <= product.rating.rate ? 'checked' : ''}>★</label>
-          </span>
-        );
-      })}
-      <span> {product.rating.count}</span>
-    </div>
+          {[...Array(5)].map((_, i) => {
+            const ratingValue = i + 1;
+            return (
+              <span key={i} >
+                <input type="radio" name="rating" value={ratingValue} />
+                <label className={ratingValue <= product.rating.rate ? 'checked' : ''}>★</label>
+              </span>
+            );
+          })}
+          <span> {product.rating.count}</span>
+        </div>
       </div>
-      
     </div>
   )
 }
