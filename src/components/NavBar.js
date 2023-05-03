9e2da1c74dfc2ef809aa73f72ba96fd7d4eedc45
Yahya493 <78732281+Yahya493@users.noreply.github.com>
2023-05-03 21:19:22 +0300

    deployment

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -12,7 +12,7 @@ export default class NavBar extends Component {
     return (
       <nav>
         <div className="nav-wrapper row">
-          <Link to="#" className="brand-logo left hide-on-small-and-down" style={{ paddingLeft: '10px' }}>FakeStore</Link>
+          <Link to="/" className="brand-logo left hide-on-small-and-down" style={{ paddingLeft: '10px' }}>FakeStore</Link>
           <ul id="nav-mobile" className="right">
             <li><Link to='/'>Home</Link></li>
             <li>
